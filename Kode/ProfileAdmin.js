b85bc945ccdbeab9902ee777116cb2d887172ef7
Bunga <rasikhahhayaa@gmail.com>
2025-03-24 00:31:13 +0700

    Perbaikan

--- Kode/ProfileAdmin.js
+++
@@ -1,6 +0,0 @@
-// Simple toggle for mobile menu
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('.menu-toggle').addEventListener('click', function() {
-        document.querySelector('.sidebar').classList.toggle('show');
-    });
-});
